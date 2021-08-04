e64cac55b788d6f3de5cbe97c1fb61a755b89c0a
Eric Lars Lee <ericlarslee@gmail.com>
2021-08-04 15:56:23 -0500

    completeted simple game

--- Number Game/ConsoleApp1/Program.cs
+++ Number Game/ConsoleApp1/Program.cs
@@ -7,6 +7,35 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess a number in between 0 and 100");
+                string userInput = Console.ReadLine();
+
+                int userNumber = int.Parse(userInput);
+
+                if (userNumber > winNum)
+                {
+                    Console.WriteLine("Your guess is too high, pick a lower number");
+                }
+                else if (userNumber < winNum)
+                {
+                    Console.WriteLine("Your guess is too low, pick a higher number");
+                }
+                else if (userNumber == winNum)
+                {
+                    Console.WriteLine("You win. Do you want a cookie");
+                    win = true;
+                }
+
+            } while (win == false);
+            Console.WriteLine("The game is over. You should leave the console now");
         }
     }
 }
